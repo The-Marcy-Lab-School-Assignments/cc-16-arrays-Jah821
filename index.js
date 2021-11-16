7f1d5cdc4d01d7d6fa2be032c609a1367826cfe6
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-11-16 15:39:52 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+const replacewithYerr = (arr, start = 0, num = arr.length - 1) => {
+    const replaced = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i >= start && i <= start + num - 1) {
+           replaced.push('Yerr')
+        }else{
+            replaced.push(arr[i])
+        }
+
+    }
+    return replaced
+}
+
+console.log(replacewithYerr(['logogogog','lgogoggolgog','gjdkjfdfdivijmv','giidfnvfn','fkjmvt'], 0, ))
